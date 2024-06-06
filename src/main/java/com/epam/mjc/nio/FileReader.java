ee549efdc519489982592b08176c387ce478d0b9
MuhammadAkbar007 <fotismenos007@mail.ru>
2024-06-06 15:08:43 +0500

    default and comments 💬

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -37,6 +37,9 @@ public class FileReader {
                     case "Phone":
                         phone = Long.parseLong(strings[1]);
                         break;
+                    default:
+                        System.out.println("Default in swith");
+                        break;
                 }
 
             }
@@ -45,32 +48,6 @@ public class FileReader {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
-//        Path path = Paths.get(file.toPath().toString());
-
-//        try {
-//
-//            Map<String, String> linesMap = new HashMap<>();
-//
-//            List<String> lines = Files.readAllLines(path, StandardCharsets.UTF_8);
-//
-//            for (String line : lines) {
-//                String[] pairs = line.split(": ", 2);
-//                if (pairs.length == 2) {
-//                    linesMap.put(pairs[0].trim(), pairs[1].trim());
-//                }
-//            }
-//
-//            String name = linesMap.get("Name");
-//            int age = Integer.parseInt(linesMap.get("Age"));
-//            String email = linesMap.get("Email");
-//            long phone = Long.parseLong(linesMap.get("Phone"));
-//
-//            return new Profile(name, age, email, phone);
-//
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
 
     }
 }
